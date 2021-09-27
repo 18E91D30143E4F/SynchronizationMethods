174fa5b57859b82062cfb4771b3410e8bd7834b8
Danil Beltyukov <beltyukov.e.danil@gmail.com>
2021-09-27 22:42:38 +0300

    Добавьте файлы проекта.

---
+++ Mutex.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading;
+
+namespace SynchronizationMethods
+{
+    public class Mutex
+    {
+        private int curId = -1;
+
+        public int Lock()
+        {
+            var id = Thread.CurrentThread.ManagedThreadId;
+            while (Interlocked.CompareExchange(ref this.curId,
+                id, -1) != -1)
+            {
+            }
+
+            return id;
+        }
+
+        public void Unlock()
+        {
+            var id = Thread.CurrentThread.ManagedThreadId;
+            Interlocked.CompareExchange(ref this.curId,
+                -1, id);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace SynchronizationMethods
+{
+    class Program
+    {
+        internal static int Var = 20;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kill std handle? y/n");
+
+            TestMutexThreads();
+
+            if (Console.ReadLine() == "y")
+            {
+                CloseStdHandle();
+            }
+
+            Console.ReadKey();
+
+        }
+
+
+        private const uint STD_INPUT_HANDLE = 0xfffffff6;
+        [DllImport("kernel32.dll")]
+        private static extern int GetStdHandle(uint nStdHandle);
+        private static void CloseStdHandle()
+        {
+            var osHandle = new OsHandle
+            {
+                Handle = (IntPtr)GetStdHandle(STD_INPUT_HANDLE)
+            };
+            osHandle.Close();
+        }
+
+        private static void TestMutexThreads()
+        {
+            const int n = 5;
+            var mutex = new Mutex();
+            var Threads = Enumerable.Range(0, n)
+                .Select(i =>
+                {
+                    var thread = new Thread(() =>
+                    {
+                        var threadId = mutex.Lock();
+                        Console.WriteLine($"Thread {threadId } lock");
+                        Var++;
+                        Thread.Sleep(400);
+                        Console.WriteLine($"Thread { threadId } unlock");
+                        mutex.Unlock();
+                    })
+                    {
+                        Name = $"Thread {i}"
+                    };
+                    thread.Start();
+
+                    return thread;
+                }).ToList();
+        }
+    }
+}
